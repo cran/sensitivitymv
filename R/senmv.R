9cca1fffb74914e200b004fc1c84129c3d20f1c7
Paul R. Rosenbaum <rosenbaum@wharton.upenn.edu>
2015-08-19 00:00:00 +0000

    version 1.3

--- R/senmv.R
+++ R/senmv.R
@@ -1,6 +1,6 @@
 senmv <-
-function (y, gamma = 1, method = NULL, inner = 0, trim = 2.5, lambda = 1/2, 
-              tau = 0, TonT = FALSE) 
+function (y, gamma = 1, method = NULL, inner = 0, trim = 2.5, lambda = 1/2,
+              tau = 0, TonT = FALSE)
     {
         if (is.vector(y)) {
             y <- y[!is.na(y)]
@@ -11,19 +11,19 @@ function (y, gamma = 1, method = NULL, inner = 0, trim = 2.5, lambda = 1/2,
         stopifnot((0 <= inner) & (inner <= trim))
         stopifnot((lambda > 0) & (lambda < 1))
         stopifnot(gamma >= 1)
-        stopifnot(min(apply(!is.na(y), 2, sum)) >= 2)
+        stopifnot(min(apply(!is.na(y), 1, sum)) >= 2)
         m <- 1
         m1 <- 1
         m2 <- 1
-        if (!is.null(method)) 
+        if (!is.null(method))
             stopifnot(is.element(method, c("h", "i", "w", "t","q","s","l")))
         wgt <- (m > 1)
         if (!is.null(method)) {
-            if (is.element(method, c("w","q","s","l"))) 
+            if (is.element(method, c("w","q","s","l")))
                 wgt <- TRUE
         }
         vc <- (sum(is.na(as.vector(y)))) > 0
-        if (wgt & vc) 
+        if (wgt & vc)
             warning("Weighting of sets is not available for matched sets of variable sizes.")
         stopifnot(FALSE == (wgt & vc))
         if (!is.null(method)) {
@@ -91,12 +91,12 @@ function (y, gamma = 1, method = NULL, inner = 0, trim = 2.5, lambda = 1/2,
                 TonT <- FALSE
             }
         }
-        if (!(tau == 0)) 
+        if (!(tau == 0))
             y[, 1] <- y[, 1] - tau
-        ms <- mscorev(y, inner = inner, trim = trim, qu = lambda, 
+        ms <- mscorev(y, inner = inner, trim = trim, qu = lambda,
                       TonT = TonT)
-        if (m > 1) 
+        if (m > 1)
             separable1v(newurks(ms, m1 = m1, m2 = m2, m = m), gamma = gamma)
-        else if (m == 1) 
+        else if (m == 1)
             separable1v(ms, gamma = gamma)
     }
